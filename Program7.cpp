14cb03b96a519813e1ea5acc829456dfc9fcf7fe
sainirock61 <44577428+sainirock61@users.noreply.github.com>
2018-11-19 00:17:10 +0530

    Add files via upload

---
+++ Program7.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int n,even=0,odd=0;
+float i,sum=0;
+int no;
+cout<<"Write a menu driven program to compute the sum of first n terms of the following series\n";
+cout<<"\n";
+cout<<"      1.1-2+3-4+5-6+7-8.....\n";
+cout<<"\n";
+cout<<"      2.1+1/2+1/3+1/4+1/5....\n";
+cout<<"\n";
+cout<<"    Enter the number(1or2) so that the sum of which program compute first:"<<" ";
+while(!(cin>>no)||(no!=1 && no!=2))
+{
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout<<"INVALID INPUT. ENTER AGAIN:";
+}
+
+
+cout<<"\n";
+switch(no)
+
+{
+case 1:
+cout<<"   Enter the number of terms to which you want the sum="<<" ";
+while(!(cin>>n)||n<=0)
+    {
+        cin.clear();
+        cin.ignore(100,'\n');
+        cout<<"INVALID INPUT. ENTER AGAIN:";
+    }
+
+cout<<"\n";
+i=1;
+while(i<=n)
+{
+if((int)i%2==0)
+{
+even=even+i;
+}
+else
+{
+odd=odd+i;
+}
+i=i+1;
+}
+cout<<"Sum of "<<n<<" terms of series is="<<" "<<(odd-even);
+
+break;
+
+
+case 2:
+cout<<"   Enter the number of terms to which you want the sum="<<" ";
+while(!(cin>>n)||n<=0)
+{
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout<<"INVALID INPUT! ENTER AGAIN";
+}
+
+cout<<"\n";
+i=1;
+while(i<=n)
+{
+sum=sum+(1.0)/i;
+i=i+1;
+}
+cout<<"Sum of "<<n<<" terms of series is="<<" "<<sum;
+break;
+
+
+}
+}
+
---
+++ Untitled1.cpp
@@ -0,0 +1,219 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,j,k,i,number,space,star,q;
+
+char d;
+cout<<"                                                        ============================\n";
+cout<<"                                                               PATTERN PROGRAM\n";
+cout<<"                                                        ============================\n";
+cout<<"\n";
+cout<<"\n";
+cout<<"                                              1.   *               2.  *                     3.  * \n";
+cout<<"                                                  **                   **                       ***\n";
+cout<<"                                                 ***                   ***                     *****\n";
+cout<<"                                                ****                   ****                   *******\n";
+cout<<"\n";
+cout<<"                                             4.    *                 5.    A                6.  A\n";
+cout<<"                                                  ***                     AB                    AB\n";
+cout<<"                                                 *****                   ABC                    ABC\n";
+cout<<"                                                  ***                   ABCD                    ABCD\n";
+cout<<"                                                   *            ";
+cout<<"\n";
+cout<<"\n";
+cout<<"                                             Enter the number of program which you want to print="<<" ";
+
+while(!(cin>>n)|| (n!=1 && n!=2 && n!=3 && n!=4 && n!=5 && n!=6) )
+{
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout<<"                                                      INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+cout<<"\n";
+
+switch(n)
+{
+case 1:
+cout<<"                                                           Enter the number of rows ="<<" ";
+while (!(cin>>number) || (number<=0))
+{
+cin.clear();
+cin.ignore(100,'\n');
+cout<<"                                                          INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+i=1;
+while(i<=number)
+{
+space=1;
+cout<<"                                                                     ";
+while(space<=(number-i))
+{
+cout<<" ";
+space=space+1;
+}
+star=1;
+while(star<=i)
+{
+cout<<"*";
+star=star+1;
+}
+cout<<"\n";
+i=i+1;
+}
+break;
+case 2:
+cout<<"                                                          Enter the number of rows ="<<" ";
+while (!(cin>>number) || (number<=0))
+{
+cin.clear();
+cin.ignore(100,'\n');
+cout<<"                                                          INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+i=1;
+while(i<=number)
+{
+cout<<"                                                                     ";
+star=1;
+while(star<=i)
+{
+cout<<"*";
+star=star+1;
+}
+cout<<"\n";
+i=i+1;
+}
+break;
+case 3:
+cout<<"                                                          Enter the number of rows ="<<" ";
+while (!(cin>>number) || (number<=0))
+{
+cin.clear();
+cin.ignore(100,'\n');
+cout<<"                                                          INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+i=1;
+while(i<=number)
+{
+space=1;
+cout<<"                                                                     ";
+while(space<=(number-i))
+{
+cout<<" ";
+space=space+1;
+}
+star=1;
+while(star<=(2*i-1))
+{
+cout<<"*";
+star=star+1;
+}
+cout<<"\n";
+i=i+1;
+}
+break;
+case 4:
+cout<<"                                                          Enter the number of rows ="<<" ";
+while (!(cin>>number) || (number<=0))
+{
+cin.clear();
+cin.ignore(100,'\n');
+cout<<"                                                          INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+i=1;
+while(i<=n)
+{
+j=1;
+cout<<"                                                                     ";
+while(j<=n-i)
+{
+cout<<" ";
+j=j+1;
+}
+k=1;
+while(k<=2*i-1)
+{
+cout<<"*";
+k=k+1;
+}
+cout<<"\n";
+i=i+1;
+}
+i=1;
+while(i<=n-1)
+{
+j=1;
+cout<<"                                                                     ";
+while(j<=i)
+{
+cout<<" ";
+j=j+1;
+}
+k=1;
+while(k<=2*(n-i)-1)
+{
+ cout<<"*";
+ k=k+1;
+}
+cout<<"\n";
+i=i+1;
+}
+break;
+case 5:
+cout<<"                                                     Enter the number of rows ="<<" ";
+while (!(cin>>number) || (number<=0))
+{
+cin.clear();
+cin.ignore(100,'\n');
+cout<<"                                                     INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+i=1;
+while(i<=number)
+{
+j=1;
+d='A';
+cout<<"                                                                     ";
+for(space=1;space<=(number-i);space++)
+{
+cout<<" ";
+}
+while(j<=i)
+{
+cout<<d;
+d=d+1;
+j=j+1;
+}
+i=i+1;
+cout<<"\n";
+}
+break;
+case 6:
+cout<<"                                                      Enter the number of rows ="<<" ";
+while (!(cin>>number) || (number<=0))
+{
+cin.clear();
+cin.ignore(100,'\n');
+cout<<"                                                      INVALID INPUT! ENTER AGAIN:"<<" ";
+}
+i=1;
+while(i<=number)
+{
+cout<<"                                                                     ";
+j=1;
+d='A';
+while(j<=i)
+{
+cout<<d;
+d=d+1;
+j=j+1;
+}
+i=i+1;
+cout<<"\n";
+}
+break;
+
+}
+}
+
+
+
