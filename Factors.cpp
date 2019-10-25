ad1019e0b6e3fe744586e764b452ac9932e8a805
ic22082000 <56989443+ic22082000@users.noreply.github.com>
2019-10-25 09:39:14 +0530

    4

--- Factors.cpp
+++ Factors.cpp
@@ -20,6 +20,46 @@ if(n%i==0)
 {
 cout<<i<<"\n";
 }
+}int main()
+{
+int i,n;
+cout<<"Program to find the factors of the given number";
+cout<<"\n";
+cout<<"\n";
+cout<<"                                        Enter the number of which you want to find the factors="<<" ";
+while(!(cin>>n) || (n<=0))
+{
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout<<"'INVALID INPUT! ENTER A POSITIVE NUMBER:"<<" ";
+}
+
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+cout<<i<<"\n";
+}
+}int main()
+{
+int i,n;
+cout<<"Program to find the factors of the given number";
+cout<<"\n";
+cout<<"\n";
+cout<<"                                        Enter the number of which you want to find the factors="<<" ";
+while(!(cin>>n) || (n<=0))
+{
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout<<"'INVALID INPUT! ENTER A POSITIVE NUMBER:"<<" ";
+}
+
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+cout<<i<<"\n";
+}
 }
 }
 
