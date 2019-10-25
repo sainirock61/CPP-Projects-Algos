5b602ec004df37903c15fb4fc880a76a0a3314c8
tanyasingh16 <53910978+tanyasingh16@users.noreply.github.com>
2019-10-25 09:33:44 +0530

    1
    added int main

--- FIBONACCI2.cpp
+++ FIBONACCI2.cpp
@@ -60,3 +60,24 @@ cout<<"\n====================================================================\n"
         return 0;
 
 }
+int main()
+{
+cout<<"\n====================================================================\n";
+    int n;
+    cout<<"ENTER THE NUMBER OF TERMS OF THE FIBONACCI SERIES :-- ";
+    while(!(cin>>n)|| (n<=0))
+        {
+            cin.clear();
+            cin.ignore(100,'\n');
+            cout<<"    !!!!!    INVALID INPUT. ENTER AGAIN :-- ";
+
+        }
+
+        cout<<"\n 0 1 ";
+
+        fibonacci(n-2);
+cout<<"\n====================================================================\n";
+
+        return 0;
+
+}
