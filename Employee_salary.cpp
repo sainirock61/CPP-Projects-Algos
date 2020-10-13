1ad60efca4446f93cb91a4cb9355fa510876be05
Jatin Sejwal <jatinsejwal10@gmail.com>
2020-10-13 20:32:25 +0530

    Update Employee_salary.cpp
    Added an option for the user to provide the output file name.

--- Employee_salary.cpp
+++ Employee_salary.cpp
@@ -10,9 +10,13 @@ int main()
  struct student s[10];
  int i,n;
 
-
-
- obj.open("data.txt",ios::out);
+ string filename = "data.txt";
+ 
+ cout << "Enter the output filename: ";
+ cin >> filename;
+ 
+ obj.open(filename,ios::out);
+ 
  cout<<"How many records to enter(max 10)"<<endl;
  cin>>n;
  for(i=0;i<n;i++)
@@ -33,8 +37,8 @@ int main()
 	}
  obj.close();
 
- obj.open("data.txt",ios::in);
- cout<<"\nAccessing from the file:\n "<<endl;
+ obj.open(filename,ios::in);
+ cout<<"\nAccessing from the file '" << filename << "':\n "<<endl;
  cout<<"\t| Emp_id\t|NAME\t|HRA\t|DA\t|Extra Allowances |\n\n";
 
  for(i=0;i<n;i++)
@@ -43,6 +47,18 @@ int main()
 	 obj.read((char*)&s[i],sizeof(s[i]));
 	 cout<<"\t "<<s[i].empid<<"\t"<<s[i].nm<<"\t"<<s[i].hra<<"\t"<<s[i].da<<"\t   "<<s[i].ext<<endl;
 	}
+	
+	cout<<"\n EMPLOYEE-"<<i+1<<":"<<endl;
+	 cout<<"Emp_id:";
+	 cin>>s[i].empid;
+	 cout<<"Name:";
+	 cin>>s[i].nm;
+	 cout<<"HRA:";
+	 cin>>s[i].hra;
+	 cout<<"DA:";
+	 cin>>s[i].da;
+	 cout<<"Extra:";
+	 cin>>s[i].ext;
 
  obj.close();
 
