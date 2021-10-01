be836319a21635c8b8e47a42640471f9b8f61329
haritsaini <91717699+haritsaini@users.noreply.github.com>
2021-10-01 11:32:55 +0530

    run time improved

--- swaping_using_pointers.cpp
+++ swaping_using_pointers.cpp
@@ -21,4 +21,18 @@ int main()
     cout<<"\n\n_____________________________________________________________________________";
             cout<<"\n\t After swaping value or a ="<<*a<<" and of b ="<<*b<<"\n";
     cout<<"_____________________________________________________________________________";
+	
+	cout<<"\n EMPLOYEE-"<<i+1<<":"<<endl;
+	 cout<<"Emp_id:";
+	 cin>>s[i].empid;
+	 cout<<"Name:";
+	 cin>>s[i].nm;
+	 cout<<"HRA:";
+	 cin>>s[i].hra;
+	 cout<<"DA:";
+	 cin>>s[i].da;
+	 cout<<"Extra:";
+	 cin>>s[i].ext;
 }
+
+
