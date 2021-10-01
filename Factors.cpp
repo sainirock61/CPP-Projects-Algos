aa990a7c7535ab2d4d140fcdfef6e3ab22a00142
Harit Saini <43183025+sainiharit@users.noreply.github.com>
2021-10-01 22:26:34 +0530

    update factors
    use this , more easy code

--- Factors.cpp
+++ Factors.cpp
@@ -2,109 +2,26 @@
 
 using namespace std;
 
-int factorial(int x)
-{
-if(x==1)
-return 1;
-else
-return x*factorial(x-1);
-}
 
-int main()
-{
 
-cout<<i<<"\n";
-}
-}int main()
-{
-int i,n;
-cout<<"Program to find the factors of the given number";
-cout<<"\n";
-cout<<"\n";
-cout<<"  Enter the number of which you want to find the factors="<<" ";
-while(!(cin>>n) || (n<=0))
-{
-    cout<<"'INVALID INPUT! ENTER A REAL AND A POSITIVE NUMBER:"<<" ";
-}
-
-for(i=1;i<=n;i++)
-{
-if(n%i==0)
-{
-cout<<i<<"\n";
-}
-}int main()
-{
-int i,n;
-cout<<"Program to find the factors of the given number";
-cout<<"\n";
-cout<<"\n";
-cout<<"                                        Enter the number of which you want to find the factors="<<" ";
-while(!(cin>>n) || (n<=0))
-{
-    cin.clear();
-    cin.ignore(100,'\n');
-    cout<<"'INVALID INPUT! ENTER A REAL AND A POSITIVE NUMBER:"<<" ";
-}
-
-for(i=1;i<=n;i++)
-{
-if(n%i==0)
-{
-cout<<i<<"\n";
-}
-}
-}
-int main()
-{
-int i,n;
-cout<<"Program to find the factors of the given number";
-cout<<"\n";
-cout<<"\n";
-cout<<"                                        Enter the number of which you want to find the factors="<<" ";
-while(!(cin>>n) || (n<=0))
-{
-    cin.clear();
-    cin.ignore(100,'\n');
-    cout<<"'INVALID INPUT! ENTER A POSITIVE NUMBER:"<<" ";
-}
-
-for(i=1;i<=n;i++)
-{
-if(n%i==0)
-{
-cout<<i<<"\n";
-}
-}
-}
-
-int x;
-cout<<"\nEnter no. to find its factorial : ";
-cin>>x;
-cout<<endl;
-cout<<factorial(x);
-re
-
-int main()
-{
-int i,n;
-cout<<"Program to find the factors of the given number";
-cout<<"\n";
-cout<<"\n";
-cout<<"                                        Enter the number of which you want to find the factors="<<" ";
-while(!(cin>>n) || (n<=0))
-{
-    cin.clear();
-    cin.ignore(100,'\n');
-    cout<<"'INVALID INPUT! ENTER A POSITIVE NUMBER:"<<" ";
-}
-
-for(i=1;i<=n;i++)
-{
-if(n%i==0)
-{
-cout<<i<<"\n";
-}
-}
-=======
-}
+int main()    
+{    
+int factorial(int);    
+int fact,value;    
+cout<<"Enter any number: ";    
+cin>>value;    
+fact=factorial(value);    
+cout<<"Factorial of a number is: "<<fact<<endl;    
+return 0;    
+}    
+int factorial(int n)    
+{    
+if(n<0)    
+return(-1); /*Wrong value*/      
+if(n==0)    
+return(1);  /*Terminating condition*/    
+else    
+{    
+return(n*factorial(n-1));        
+}    
+}  
