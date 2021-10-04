5c68d92e10a4249a4651236c615d05e98e3b727a
imgroot2020 <72227911+imgroot2020@users.noreply.github.com>
2021-10-04 13:12:29 +0530

    Update Factors2.cpp
    Reversing a Stack with the help of another empty Stack

--- Factors2.cpp
+++ Factors2.cpp
@@ -1,32 +1,66 @@
-#include<iostream>
+// C++ program to reverse a stack
+// by using an extra stack
+#include <bits/stdc++.h>
 using namespace std;
+
+// Function to transfer elements of
+// the stack s1 to the stack s2
+void transfer(stack<int>& s1,
+			stack<int>& s2, int n)
+{
+	for (int i = 0; i < n; i++) {
+
+		// Store the top element
+		// in a temporary variable
+		int temp = s1.top();
+
+		// Pop out of the stack
+		s1.pop();
+
+		// Push it into s2
+		s2.push(temp);
+	}
+}
+
+// Function to reverse a stack using another stack
+void reverse_stack_by_using_extra_stack(stack<int>& s,
+										int n)
+{
+	stack<int> s2;
+
+	for (int i = 0; i < n; i++) {
+
+		// Store the top element
+		// of the given stack
+		int x = s.top();
+
+		// Pop that element
+		// out of the stack
+		s.pop();
+
+		transfer(s, s2, n - i - 1);
+		s.push(x);
+		transfer(s2, s, n - i - 1);
+	}
+}
+
+// Driver Code
 int main()
 {
-int i,n;
-cout<<"Program to find the factors of the given number";
-cout<<"\n";
-cout<<"\n";
-cout<<"                                        Enter the number of which you want to find the factors="<<" ";
-while(!(cin>>n) || (n<=0))
-{
-    cin.clear();
-    cin.ignore(100,'\n');
-    cout<<"'INVALID INPUT! ENTER A POSITIVE NUMBER:"<<" ";
-}
+	int n = 5;
 
-for(i=1;i<=n;i++)
-{
-if(n%i==0)
-{
-cout<<i<<"\n";
-}
-}
-}
-<div class="col-xs-12 col-md-6 col-lg-6">
-            <div class="about-item">
-              <img class="img-fluid" src="assets/img/about/img1.jpg" alt="">
-              <div class="about-text">
+	stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	s.push(5);
 
-              <div class="about-text">
+	reverse_stack_by_using_extra_stack(s, n);
 
-    
+	for (int i = 0; i < n; i++) {
+		cout << s.top() << " ";
+		s.pop();
+	}
+	return 0;
+}
