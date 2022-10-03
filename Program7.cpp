f725025effa06d2bf8e794f2db77787ed9e65ed1
sardar7saini <56949831+sardar7saini@users.noreply.github.com>
2022-10-03 13:43:12 +0530

    Merge Sort for Doubly Linked List

--- Program7.cpp
+++ Program7.cpp
@@ -82,3 +82,130 @@ break;
 }
 }
 
+// C# program to implement merge
+// sort in singly linked list
+
+using System;
+
+// Linked List Class
+public class LinkedList
+{
+
+	Node head; // head of list
+
+	/* Node Class */
+	class Node
+	{
+
+		public int data;
+		public Node next, prev;
+
+		// Constructor to create a new node
+		public Node(int d)
+		{
+			data = d;
+			next = prev = null;
+		}
+	}
+
+	void print(Node node)
+	{
+		Node temp = node;
+		Console.WriteLine("Forward Traversal" +
+						"using next pointer");
+		while (node != null)
+		{
+			Console.Write(node.data + " ");
+			temp = node;
+			node = node.next;
+		}
+		Console.WriteLine("\nBackward Traversal" +
+							"using prev pointer");
+		while (temp != null)
+		{
+			Console.Write(temp.data + " ");
+			temp = temp.prev;
+		}
+	}
+
+	// Split a doubly linked list (DLL)
+	// into 2 DLLs of half sizes
+	Node split(Node head)
+	{
+		Node fast = head, slow = head;
+		while (fast.next != null &&
+				fast.next.next != null)
+		{
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		Node temp = slow.next;
+		slow.next = null;
+		return temp;
+	}
+
+	Node mergeSort(Node node)
+	{
+		if (node == null || node.next == null)
+		{
+			return node;
+		}
+		Node second = split(node);
+
+		// Recur for left and right halves
+		node = mergeSort(node);
+		second = mergeSort(second);
+
+		// Merge the two sorted halves
+		return merge(node, second);
+	}
+
+	// Function to merge two linked lists
+	Node merge(Node first, Node second)
+	{
+		// If first linked list is empty
+		if (first == null) {
+			return second;
+		}
+
+		// If second linked list is empty
+		if (second == null)
+		{
+			return first;
+		}
+
+		// Pick the smaller value
+		if (first.data < second.data)
+		{
+			first.next = merge(first.next, second);
+			first.next.prev = first;
+			first.prev = null;
+			return first;
+		}
+		else
+		{
+			second.next = merge(first, second.next);
+			second.next.prev = second;
+			second.prev = null;
+			return second;
+		}
+	}
+
+	// Driver code
+	public static void Main(String[] args)
+	{
+
+		LinkedList list = new LinkedList();
+		list.head = new Node(10);
+		list.head.next = new Node(30);
+		list.head.next.next = new Node(3);
+		list.head.next.next.next = new Node(4);
+		list.head.next.next.next.next = new Node(20);
+		list.head.next.next.next.next.next = new Node(5);
+		
+		Node node = null;
+		node = list.mergeSort(list.head);
+		Console.WriteLine("Linked list after sorting :");
+		list.print(node);
+	}
+}
