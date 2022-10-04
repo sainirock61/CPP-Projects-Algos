460f909ac57dac92d24acbb4f1a49b339e3c85b5
backup0202 <92112843+backup0202@users.noreply.github.com>
2022-10-04 12:01:06 +0530

    Update Program8.cpp
    This will work now.

--- Program8.cpp
+++ Program8.cpp
@@ -1,46 +1,5 @@
 #include<iostream>
 using namespace std;
-
-int main()
-{
-int n,i,j,count;
-cout<<"Program to print all prime number less than n\n";
-cout<<"\n";
-cout<<"\n";
-cout<<"Enter n so that all prime number before n should be computed:"<<" ";
-cin>>n;
-i=2;
-	
-int main()
-{
-int n,i,j,count;
-cout<<"Program to print all prime number less than n\n";
-cout<<"\n";
-cout<<"\n";
-cout<<"Enter n so that all prime number before n should be computed:"<<" ";
-cin>>n;
-i=2;
-while(i<n)
-{
-count=0;
-j=2;
-while(j<i)
-{
-if(i%j==0)
-{
-count++;
-}
-j=j+1;
-}
-if(count==0)
-{
- cout<<i<<" ";
-}
-i=i+1;
-}
-}
-	
-	
 	
 // C# program to delete every k-th Node
 // of a singly linked list.
