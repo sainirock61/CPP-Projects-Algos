a256de350945bac2e9da2b755cbcbc736791cb6a
backup0202 <92112843+backup0202@users.noreply.github.com>
2022-10-04 11:59:17 +0530

    Update swaping_using_pointers.cpp

--- swaping_using_pointers.cpp
+++ swaping_using_pointers.cpp
@@ -36,3 +36,25 @@ int main()
 }
 
 
+// Solution 2
+
+#include <iostream>
+using namespace std;
+void swap(int *num1, int *num2) {
+   int temp;
+   temp = *num1;
+   *num1 = *num2;
+   *num2 = temp;
+}
+int main() {
+   int num1, num2;
+   cout<<"\nEnter the first number : ";
+   cin>>num1;
+   cout<<"\nEnter the Second number : ";
+   cin>>num2;
+   swap(&num1, &num2);
+   cout<<"\nAfter Swapping, we have observed the following changes";
+   cout<<"\nFirst number : "<< num1;
+   cout<<"\nSecond number: "<<num2;
+}
+
